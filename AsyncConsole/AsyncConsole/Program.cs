62eafcea3d5bcf1970b27a398a3f7eb8fe67bf96
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2025-01-12 09:25:19 +0100

    StatsCounter, ASsync, Multithreading

---
+++ AsyncConsole/AsyncConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AsyncConsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("http://www.apple.com");
+            Console.WriteLine("Apple's home page {0:0N} bytes", response.Content.Headers.ContentLength);
+        }
+    }
+}
